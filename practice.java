0161618d2f0986ce589ca0ed3255d3a442c0af25
NzuzoHamza <36505735+NzuzoHamza@users.noreply.github.com>
2019-10-07 23:14:01 +0200

    Add files via upload

--- practice.java
+++ practice.java
@@ -7,12 +7,19 @@ private List<String> cards;
 public practice(List<String> cards){
 this.cards = cards;
 }
+
+
+
 public List<String> getcards(){
 return cards;
 }
+
+
+
 public void bankerCall(){
 	
 }
+
 
 
 
@@ -23,47 +30,53 @@ return String.format("%s", cards);
 
 
 
+
+
 public static void main(String[] args){
 Scanner in = new Scanner(System.in);
-int count = 0;
 String[] cards ={"card1","card2","card3","card4","card5","card6","card7","card8","card9","card10","card11","card12","card13"};
 List<String>Linkedcards = new ArrayList<>();
 for(int i =0;i<cards.length;i++){
 Linkedcards.add(cards[i]);
 }
+
+
 String[] money ={"R100","200","R300","R1","R5000","R10000","R40000","R60000","75000","100000",
 "250000","1000000","R2"};
 List<String>listMoney = new ArrayList<>();
-while(count<13){
 for(int i =0;i<money.length;i++){
 listMoney.add(money[i]);
 Collections.shuffle(listMoney);
 }
 
 
+
+
+
 practice p1 = new practice(Linkedcards);
 System.out.println(p1);
 
-for(int i =0;i<listMoney.size();i++){
+for(int i =0;i<cards.length;i++){
 System.out.println("pick a card");
-String num = in.nextLine();
+String num = in.nextLine().toLowerCase();
 if((p1.getcards()).contains(num)){
 String cash = listMoney.get((p1.getcards()).indexOf(num));
 System.out.println(cash);
 }
 
+//for(int i =0;i<listMoney.size();i++){
+
+
 if((p1.getcards()).contains(num)){
-    listMoney.remove((p1.getcards()).indexOf(num));
+	listMoney.remove((p1.getcards()).indexOf(num));
 	String card = Linkedcards.remove((p1.getcards()).indexOf(num));
-	
 	System.out.println("available cards: " + p1);
 }
+
 
 else{
 System.out.println("not found");
 }
-}
-count++;
 }
 
 
